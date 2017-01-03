97eba647739d5299edd730dbdbab6f505f6919b8
Alexander Sigatchov <asigatchov@gmail.com>
2017-01-03 07:47:03 +0300

    fix path demo log

--- demo_log_generate.rb
+++ demo_log_generate.rb
@@ -1,7 +1,5 @@
 
-#require 'date'
-
-log = File.open('./data/access.log', "w")
+log = File.open('/tmp/access.log', "w")
 IO.popen("zcat data/my.access.log.1.gz").each do |f|
   log.write ( f.gsub(%r{\[\d+/Dec/2016}, Time.now.strftime("[%d/%b/%Y")))
   sleep 0.0009
